c26e3e26d9c3a667e8af037dc707d2ac4a43761d
shadowfeind <travestroy007@gmail.com>
2024-04-16 11:06:43 +0545

    auth.ts callbacks weired bug fixed with any type

--- auth.ts
+++ auth.ts
@@ -2,19 +2,29 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
+import { getUserById } from "./data/user"
 
 // db session doesnot work in edge
 //we are using auth.config as prisma does not support edge
 export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth({
   callbacks: {
-    async session({ session, token }){
+    async session({ session, token }: any){
       if(token.sub && session.user){
         session.user.id = token.sub
       }
+      if(token.role && session.user){
+        session.user.role = token.role
+      }
       return session
-    }
-    ,
+    },
     async jwt ({token}){
+      if(!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+      if(!existingUser) return token 
+
+      token.role = existingUser.role 
+
       return token
     }
   },
